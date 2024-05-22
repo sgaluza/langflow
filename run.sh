a58f4e2870cfe8c04560b5856a818a75f68aae87
LeximoKO <155750923+LeximoKO@users.noreply.github.com>
2024-05-22 12:05:52 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
-
-
 set -e
 echo "Starting run.sh at $(date)" >> run.log
 while true
 do
     python run_flow.py >> run.log 2>&1
-
-   
     if [ $? -eq 0 ]; then
       echo "run_flow.py completed successfully" >> run.log
     else
       echo "run_flow.py failed with exit code $?" >> run.log
     fi
-
-    
     sleep 60
 done
-
-
 echo "Finished run.sh at $(date)" >> run.log
